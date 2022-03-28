7b63533707a28584fd3ca9492066814c02c70012
Leonid Fedorov <leonid.fedorov@mariadb.com>
2022-03-28 14:26:02 +0000

    Random http GET request from user defined function

---
+++ dbcon/mysql/columnstore_dataload.cpp
@@ -0,0 +1,90 @@
+/* Copyright (C) 2021 MariaDB Corporation
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+*/
+
+#include <curl/curl.h>
+
+#define NEED_CALPONT_INTERFACE
+#define PREFER_MY_CONFIG_H 1
+#include "ha_mcs_impl.h"
+#include "ha_mcs_impl_if.h"
+using namespace cal_impl_if;
+
+#include "errorcodes.h"
+#include "idberrorinfo.h"
+#include "errorids.h"
+using namespace logging;
+
+#include "columnstoreversion.h"
+#include "ha_mcs_sysvars.h"
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+extern "C"
+{
+  struct InitData
+  {
+    CURL* curl;
+  };
+
+  void columnstore_dataload_deinit(UDF_INIT* initid)
+  {
+    InitData* initData = (InitData*)(initid->ptr);
+    curl_easy_cleanup(initData->curl);
+  }
+
+  const char* columnstore_dataload(UDF_INIT* initid, UDF_ARGS* /*args*/, char* result,
+                                   unsigned long* length, char* /*is_null*/, char* /*error*/)
+  {
+    InitData* initData = (InitData*)(initid->ptr);
+    CURLcode res;
+    std::string readBuffer;
+
+    if (initData->curl)
+    {
+      curl_easy_setopt(initData->curl, CURLOPT_URL, "https://gnu.terminalroot.com.br/ip.php");
+      curl_easy_setopt(initData->curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+      curl_easy_setopt(initData->curl, CURLOPT_WRITEDATA, &readBuffer);
+      res = curl_easy_perform(initData->curl);
+    }
+
+    (void)res;
+
+    strcpy(result, readBuffer.c_str());
+    *length = readBuffer.size();
+
+    return result;
+  }
+
+  my_bool columnstore_dataload_init(UDF_INIT* initid, UDF_ARGS* args, char* message)
+  {
+    InitData* initData = new InitData;
+    initData->curl = curl_easy_init();
+    initid->ptr = (char*)(initData);
+
+    if (args->arg_count != 0)
+    {
+      strcpy(message, "COLUMNSTORE_DATALOAD() takes no arguments");
+      return 1;
+    }
+
+    return 0;
+  }
+}
